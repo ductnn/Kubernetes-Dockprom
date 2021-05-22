225b226ee275372bd5faf64d013cfe3495876256
ductnn <trannhuduc531998@gmail.com>
2021-05-23 02:53:33 +0700

    Set env

--- run.sh
+++ run.sh
@@ -1,12 +1,27 @@
+set -a
+source .env
+
 # Create namespace
-kubectl create -f namespace.yaml
+envsubst < namespace.yaml | kubectl create -f -
+# kubectl create -f namespace.yaml
 
 # Prometheus
-kubectl create -k ./prometheus -n monitoring
+envsubst < ./prometheus/alert-rules-configmap.yaml | kubectl create -f -
+envsubst < ./prometheus/cluster-role.yaml | kubectl create -f -
+envsubst < ./prometheus/configmap.yaml | kubectl create -f -
+envsubst < ./prometheus/deployment.yaml | kubectl create -f -
+envsubst < ./prometheus/service.yaml | kubectl create -f -
+# kubectl create -k ./prometheus -n monitoring
 
 # Node exporter
+envsubst < ./node_exporter/daemonset.yaml | kubectl create -f -
+envsubst < ./node_exporter/service.yaml | kubectl create -f -
+# kubectl create -k ./node_exporter -n monitoring
 
 # Alert manager
 
 # Grafana
-
+envsubst < ./grafana/configmap.yaml | kubectl create -f -
+envsubst < ./grafana/deployment.yaml | kubectl create -f -
+envsubst < ./grafana/service.yaml| kubectl create -f -
+# kubectl create -k ./grafana -n monitoring
